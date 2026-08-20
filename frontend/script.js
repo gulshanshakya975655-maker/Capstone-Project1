40e62937575d6564df725f69752fb72d93ed9e26
gulshanshakya975655 <gulshanshakya975655@gmail.com>
2026-08-20 08:23:33 +0530

    add render backend url

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,7 @@
 console.log("TaskFlow script.js loaded");
 
-const API_URL = "http://127.0.0.1:8000";
+// const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://capstone-project1-ovg9.onrender.com";
 
 // =====================================================
 // ELEMENTS
